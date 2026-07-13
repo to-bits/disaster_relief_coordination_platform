1502dd17a77d5e5e52edadcb796fd7d6cbf67054
to-bits <tonoysen331@gmail.com>
2026-07-13 22:33:44 +0600

    Fixed merge conflict in AuthController

--- drcp/src/main/java/com/drcp/controller/auth/AuthController.java
+++ drcp/src/main/java/com/drcp/controller/auth/AuthController.java
@@ -1,89 +1,31 @@
 package com.drcp.controller.auth;
 
-<<<<<<< HEAD
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-
-@Controller
-public class AuthController {
-
-
-    @GetMapping("/")
-    public String home(){
-
-        return "index";
-=======
-
 import com.drcp.dto.request.RegisterRequest;
 import com.drcp.dto.response.UserResponse;
 import com.drcp.service.interfaces.UserService;
-
-
 import jakarta.validation.Valid;
-
-
 import lombok.RequiredArgsConstructor;
-
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-
-
     private final UserService userService;
-
-
-
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(
-
             @Valid
             @RequestBody RegisterRequest request
-
     ){
-
 
         UserResponse response =
                 userService.registerUser(request);
 
-
-
         return ResponseEntity.ok(response);
->>>>>>> feature-backend
 
     }
 
-
-<<<<<<< HEAD
-
-    @GetMapping("/login")
-    public String login(){
-
-        return "auth/login";
-
-    }
-
-
-
-    @GetMapping("/register")
-    public String register(){
-
-        return "auth/register";
-
-    }
-
-
-=======
->>>>>>> feature-backend
 }
